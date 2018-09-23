dece66d01037e9efeb36642aae2b9e5ec8344aab
Talat UYARER <talat@uyarer.com>
2018-09-22 18:38:15 -0700

    Updated Validator Job

--- src/main/java/edu/itu/csc550/homework/ValidatorJob.java
+++ src/main/java/edu/itu/csc550/homework/ValidatorJob.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -15,7 +16,7 @@ public class ValidatorJob extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         Job job = Job.getInstance(getConf());
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setJobName("homework-validator");
         job.setJarByClass(ValidatorJob.class);
@@ -27,6 +28,7 @@ public class ValidatorJob extends Configured implements Tool {
         job.setNumReduceTasks(1);
         // force a single split
         FileInputFormat.setMinInputSplitSize(job, Long.MAX_VALUE);
+        job.setOutputFormatClass(TextOutputFormat.class);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
--- src/main/java/edu/itu/csc550/homework/ValidatorMapper.java
+++ src/main/java/edu/itu/csc550/homework/ValidatorMapper.java
@@ -1,33 +1,36 @@
 package edu.itu.csc550.homework;
 
-import com.sun.imageio.spi.FileImageInputStreamSpi;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-import org.apache.hadoop.util.PureJavaCrc32;
 
 import java.io.IOException;
 
-public class ValidatorMapper extends Mapper<Text,Text, Text, Text> {
+public class ValidatorMapper extends Mapper<Object, Text, Text, Text> {
 
     private Text previousKey;
     private String filename;
 
-    public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+    public void setup(Context context) {
+    	System.out.println("Test");
+    }
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         //Part file is starting
+    	String[] line = value.toString().split("\\s");
+    	Text linekey = new Text(line[0]);
+    	
         if(previousKey == null){
             //Initialize variables
             filename = ((FileSplit)context.getInputSplit()).getPath().getName();
-            context.write(new Text(filename + ":start"), key);
+            context.write(new Text(filename + ":start"), new Text(line[0]));
             previousKey = new Text();
         }else{
-            if(key.compareTo(previousKey) < 0){
-                context.write(new Text("error"), new Text("filename:" + filename));
+            if(linekey.compareTo(previousKey) < 0){
+                context.write(new Text("error"), new Text("filename: " + filename + " previousKey: "+ previousKey + " lineKey: "+ linekey));
             }
         }
 
-        previousKey.set(key);
+        previousKey.set(linekey);
     }
 
     @Override
