363ba58507d1a6005e4457171feee9bb7b5246f5
Talat UYARER <talat@uyarer.com>
2018-10-25 14:38:24 -0700

    Fixed ordering bug

--- src/main/java/edu/itu/csc550/homework/ValidatorMapper.java
+++ src/main/java/edu/itu/csc550/homework/ValidatorMapper.java
@@ -1,5 +1,6 @@
 package edu.itu.csc550.homework;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
@@ -8,36 +9,33 @@ import java.io.IOException;
 
 public class ValidatorMapper extends Mapper<Object, Text, Text, Text> {
 
-    private Text previousKey;
+    private LongWritable previousKey;
     private String filename;
 
-    public void setup(Context context) {
-    	System.out.println("Test");
-    }
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         //Part file is starting
     	String[] line = value.toString().split("\\s");
-    	Text linekey = new Text(line[0]);
+    	LongWritable linekey = new LongWritable(Long.parseLong(line[0]));
     	
         if(previousKey == null){
             //Initialize variables
             filename = ((FileSplit)context.getInputSplit()).getPath().getName();
-            context.write(new Text(filename + ":start"), new Text(line[0]));
-            previousKey = new Text();
+            context.write(new Text(filename + ":begin"), new Text(line[0]));
+            previousKey = new LongWritable();
         }else{
             if(linekey.compareTo(previousKey) < 0){
-                context.write(new Text("error"), new Text("filename: " + filename + " previousKey: "+ previousKey + " lineKey: "+ linekey));
+                context.write(new Text("error"), new Text("filename: " + filename + " previousKey: "+ previousKey + " lineKey: "+ linekey.toString()));
             }
         }
 
-        previousKey.set(linekey);
+        previousKey.set(linekey.get());
     }
 
     @Override
     public void cleanup(Context context)
             throws IOException, InterruptedException  {
         if (previousKey != null) {
-            context.write(new Text(filename + ":end"), previousKey);
+            context.write(new Text(filename + ":end"), new Text(previousKey.toString()));
         }
     }
 
